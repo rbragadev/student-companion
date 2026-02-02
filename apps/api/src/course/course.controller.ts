9e6e58d6bad2e0b9f9aab45e03a3cec5bdc1cc01
rbragadev <raffael.info@gmail.com>
2026-02-02 19:11:00 -0300

    feat: implement Course module with CRUD operations; add CreateCourseDto and integrate Prisma for database interactions

--- apps/api/src/course/course.controller.ts
+++ apps/api/src/course/course.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CourseService } from './course.service';
+import { CreateCourseDto } from './dto/create-course.dto';
 
 @Controller('course')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
+
+  @Post()
+  async createCourse(@Body() createCourseDto: CreateCourseDto) {
+    const course = await this.courseService.createCourse(createCourseDto);
+    return course;
+  }
+
+  @Get()
+  async getAllCourses() {
+    const courses = await this.courseService.getAllCourses();
+    return courses;
+  }
+
+  @Get(':id')
+  async getCourseById(@Param('id') id: string) {
+    const course = await this.courseService.getCourseById(id);
+    return course;
+  }
 }
--- apps/api/src/course/course.service.ts
+++ apps/api/src/course/course.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createCourse(data: CreateCourseDto) {
+    return this.prisma.course.create({
+      data: { ...data, id: randomUUID() },
+      include: {
+        school: true,
+      },
+    });
+  }
+
+  async getAllCourses() {
+    return this.prisma.course.findMany({
+      include: {
+        school: true,
+      },
+      orderBy: {
+        rating: 'desc',
+      },
+    });
+  }
+
+  async getCourseById(id: string) {
+    return this.prisma.course.findUnique({
+      where: { id },
+      include: {
+        school: true,
+      },
+    });
+  }
+}
